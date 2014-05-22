789d131523cfbd984bd2176f47daa1d96b19cef6
adrian1nagy <adrian1nagy@yahoo.com>
2014-05-22 13:12:34 +0300

    moving to emmhub

---
+++ conf.js
@@ -0,0 +1,3 @@
+(function(){
+	this.EMMHUB_API_URL = 'http://emmhub.nutrina.webfactional.com/api/testmobile/';
+}).call(this);
---
+++ setup-data.js
@@ -0,0 +1,50 @@
+(function() {
+        this.EMMHUB_SETUP_DATA = {
+                "member_types": {
+                        "100": {
+                                "name": {
+                                        "de": "Familie",
+                                        "en": "Familie"
+                                },
+                                "children": {
+                                        "101": {
+                                                "name": {
+                                                        "de": "Kind 1",
+                                                        "en": "Kind 1"
+                                                }
+                                        },
+                                        "102": {
+                                                "name": {
+                                                        "de": "Kind 2",
+                                                        "en": "Kind 2"
+                                                }
+                                        },
+                                        "103": {
+                                                "name": {
+                                                        "de": "Kind 3",
+                                                        "en": "Kind 3"
+                                                }
+                                        }
+                                }
+                        }
+                },
+                "org_structure": {
+                        "levels": [{
+                                "name": {
+                                        "en": "Level 0"
+                                }
+                        }, {
+                                "name": {
+                                        "en": "Level 1"
+                                }
+                        }, {
+                                "name": {
+                                        "en": "Level 2"
+                                }
+                        }],
+                        "type": 2
+                },
+                "member_field_permissions": {},
+                "member_fields": {}
+        }
+}).call(this);
